b5ca2e1746b5e2637bb092d780ec99ed9e2236c4
Shannen Grimes <shannengrimes@yahoo.com>
2020-01-18 22:08:27 -0600

    Replaced friend-finder app with FriendFinder, due to Github sync issues

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friends = [
+  {
+    name: "Billy Bob",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+
+  {
+    name: "Jim Bob",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+
+  {
+    name: "Betty Lou",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+
+  {
+    name: "Dixie",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [4, 3, 5, 1, 1, 5, 5, 1, 1, 1]
+  },
+
+  {
+    name: "Bambi",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 2, 1, 3, 4, 1, 3, 3, 4]
+  },
+
+  {
+    name: "Bubba",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 1, 5, 5, 5, 5, 5, 5, 5, 1]
+  },
+
+  {
+    name: "Cletus",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 1, 5, 1, 3, 2, 5, 1, 1, 5]
+  },
+
+  {
+    name: "Scooter",
+    photo:
+      "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+    scores: [5, 1, 5, 1, 5, 5, 1, 1, 5, 1]
+  }
+];
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,53 @@
+// Pull in dependencies
+var path = require('path');
+
+// Import the list of friends
+var friends = require('../data/friends.js');
+
+// Export routes
+module.exports = function(app) {
+	// Total friends
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
+
+	// Add new friend
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var userInput = req.body;
+		// console.log('userInput = ' + JSON.stringify(userInput));
+
+		var userResponses = userInput.scores;
+		// console.log('userResponses = ' + userResponses);
+
+		// Calculate friend match
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000; 
+
+		// Loop through existing friends in the list
+		for (var i = 0; i < friends.length; i++) {
+			// console.log('friend = ' + JSON.stringify(friends[i]));
+
+			// Calculate differences for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			// console.log('diff = ' + diff);
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		// Add new user
+		friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
+};
